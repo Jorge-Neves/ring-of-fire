726d6049d75134f644a5d52e560a24e5de3f589f
Jorge-Neves <paulo.jorge.ngs@gmail.com>
2021-12-29 16:47:28 +0000

    first commit

---
+++ src/Dev/Home.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+
+function Home() {
+    
+    
+
+return (
+    <div>
+        <h1>Home - dev</h1>
+    </div>
+)
+
+} 
+
+
+
+
+export default Home;
---
+++ src/Stable/Home.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+
+function Home() {
+    
+    const [deck, setDeck]
+    
+
+return (
+    <div>
+        <h1>Home - stable</h1>
+    </div>
+)
+
+} 
+
+
+
+
+export default Home;
